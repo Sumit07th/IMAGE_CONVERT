9a2fc57687b236ab23f84965657a49e61c9e5ef5
Sumit Kumar <108703831+Sumit07th@users.noreply.github.com>
2024-09-25 13:00:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS Middleware (optional, if needed for cross-origin requests)
 app.use(cors({
-    origin: "https://image-craft-weld.vercel.app",
+    origin: "https://image-craft-app.netlify.app",
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
